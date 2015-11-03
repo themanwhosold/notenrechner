5ddabddc5ef40149a3bd84c002fb254dc106b24a
Markus Oesterle <markusoesterle@gmx.de>
2015-11-03 11:30:05 +0100

    EJB für die Notenberechnung hinzugefuegt Signed-off-by:Markus Oesterle <markusoesterle@gmx.de>

---
+++ src/main/java/de/hof/se2/sessionBean/BerechnungNoten.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hof.se2.sessionBean;
+
+import javax.ejb.Stateless;
+
+/**
+ *Logik für die Berechnung der Noten anhand der in der DB angegebenen Verhältnisse
+ * @author markus
+ * @version 0.1
+ * @since 03.11.2015
+ */
+@Stateless
+public class BerechnungNoten implements BerechnungNotenLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
