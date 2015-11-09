b6be7f751b56445f663049bd2e214be6fd62f3fb
Schmobi <tobias.schmidbauer@hof-university.de>
2015-11-09 15:21:09 +0100

    Login funzt halt so

--- src/main/java/de/hof/se2/managedBean/OutForStudents.java
+++ src/main/java/de/hof/se2/managedBean/OutForStudents.java
@@ -31,20 +31,17 @@ import org.w3c.dom.Document;
 @Dependent
 
 public class OutForStudents implements Serializable {
-<<<<<<< HEAD
-    @Default Document doc;
-    @LoggedIn UserDaten user;
-=======
-    @Current Document doc;
-    @LoggedIn User user;
-    
-		@EJB
+
+    @Default
+    Document doc;
+    @LoggedIn
+    UserDaten user;
+
+    @EJB
     private BerechnungNotenLocal berechnungNoten;
 
->>>>>>> fa6b0e81220cf55e5c492c32b42d4fdb31b12188
     @PersistenceContext
     EntityManager em;
-    
 
     /**
      * Creates a new instance of OutForStudents
@@ -54,6 +51,7 @@ public class OutForStudents implements Serializable {
 
     /**
      * Gibt eine Liste der Noten für den jeweiligen Studierenden zurück
+     *
      * @author markus
      * @version 0.1
      * @since 03.11.2015
@@ -66,52 +64,51 @@ public class OutForStudents implements Serializable {
         liste_noten_student = (List<Noten>) em.createNativeQuery("select * from noten where Matrikelnr = " + user.getUsername(), Noten.class).getResultList();
         return liste_noten_student;
     }
-    
 
     /**
      * @author max
      * @param idStudienfach
-     * @return Arithmetisches Mittel des Studiengangs aus der berechnungNoten Bean
+     * @return Arithmetisches Mittel des Studiengangs aus der berechnungNoten
+     * Bean
      */
     @Named
-    public double getArithmetischesMittel(int idStudienfach){
+    public double getArithmetischesMittel(int idStudienfach) {
         return this.berechnungNoten.getArithmethischesMittel(idStudienfach);
     }
+
     /**
      * @author max
      * @param idStudienfach
      * @return Varianz des Studiengangs aus der berechnungNoten Bean
      */
     @Named
-    public double getVarianz(int idStudienfach){
+    public double getVarianz(int idStudienfach) {
         return this.berechnungNoten.getVarianz(idStudienfach);
     }
+
     /**
      * @author max
      * @param idStudienfach
      * @return Standardabweichung des Studiengangs aus der berechnungNoten Bean
      */
     @Named
-    public double getStandardabweichung(int idStudienfach){
+    public double getStandardabweichung(int idStudienfach) {
         return this.berechnungNoten.getStandardabweichung(idStudienfach);
     }
-    
+
     /**
      * @author max
      * @param idStudienfach
      * @return Median des Studiengangs aus der berechnungNoten Bean
      */
     @Named
-    public int getMedian(int idStudienfach){
+    public int getMedian(int idStudienfach) {
         return this.berechnungNoten.getMedian(idStudienfach);
     }
-    
 
-    
     public String getHallo() {
         String a = berechnungNoten.getHello();
         return a;
     }
-
 
 }
